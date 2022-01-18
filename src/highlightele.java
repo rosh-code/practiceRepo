212d53888d2316cdbe40e62135a5a0e5f8650260
mottuanandtrica <mottu.anand@trica.co>
2022-01-18 19:35:31 +0530

    pushing initial test source code

---
+++ src/highlightele.java
@@ -0,0 +1,12 @@
+
+public class highlightele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubs
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+
+	}
+
+}
